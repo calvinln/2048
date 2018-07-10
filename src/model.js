52007d31db53e15faea6cb0e48d286467c3c79bb
Calvin Nguyen <me@calvinln.com>
2018-07-09 23:11:37 -0700

    Remove dummy test code.

--- src/model.js
+++ src/model.js
@@ -1,8 +1,3 @@
-// TODO: delete this, for temporary testing only.
-export function add(a, b) {
-  return a + b;
-}
-
 export default class Game {
   // Public methods
   constructor(addHandler, moveHandler, mergeHandler) {
